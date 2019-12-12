6a9a115ece95a0f9fb850900a087d654b6436be9
yuvalbl <yuval.bar.levi@gmail.com>
2019-12-12 13:55:52 +0200

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
-let log = [];
-
+app.use(cors());
 app.use(express.json());
+
+let log = [];
 
 app.get('/reset', (req, res) => {
     log = [];
